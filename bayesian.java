0187319d2fa54a7ae7a13c979f25cd28427a35bc
farr16 <msamfarr@outlook.com>
2017-02-11 23:54:42 -0800

    Adds prompts for user input for titles of the two variables.

--- bayesian.java
+++ bayesian.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Takes input from user about a probability problem and solves it by applying Bayes' Theorem
  *
@@ -9,5 +11,17 @@ public class Bayesian
 	{
 		System.out.println("\nWelcome to the Bayes Theorem Calculator");
 		System.out.println("This will calculate P(A|B), or the probability of A being true, given that B is true");
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("\nPlease input a name for A (recommended length: between 1 and 5 characters)");
+		System.out.print("A: ");
+		String aliasA = input.next();
+		
+		System.out.println("\nPlease input a name for B (recommended length: between 1 and 5 characters)");
+		System.out.print("B: ");
+		String aliasB = input.next();
+		
+		System.out.println("\nSolving for P(" + aliasA + " | " + aliasB + ")");
 	}
 }
