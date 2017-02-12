ae29e4243ae6f4bafec7c475b79fa096f4c42b9e
farr16 <msamfarr@outlook.com>
2017-02-11 23:51:59 -0800

    Initial commit of bayesian. Contains one file, bayesian.java, which prints the intent of the program.

---
+++ bayesian.java
@@ -0,0 +1,13 @@
+/**
+ * Takes input from user about a probability problem and solves it by applying Bayes' Theorem
+ *
+ * @author       Matthew Farr
+ */
+public class Bayesian
+{
+	public static void main(String[] args)
+	{
+		System.out.println("\nWelcome to the Bayes Theorem Calculator");
+		System.out.println("This will calculate P(A|B), or the probability of A being true, given that B is true");
+	}
+}
